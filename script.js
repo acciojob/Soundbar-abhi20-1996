d911954ba7d536eb3a5aa2a53fb4869cddb6fbfd
abhi20-1996 <110686880+abhi20-1996@users.noreply.github.com>
2023-04-14 10:39:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let myAudio=document.getElementById("myAudio");
+	function playAudio() {
+		myAudio.play();
+	}
+    function pauseAudio() {
+    	myAudio.pause();
+    }
